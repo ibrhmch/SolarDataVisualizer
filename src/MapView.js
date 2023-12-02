da4710a5bcb2f6a78e37b4d77961a551e21720b9
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2023-12-01 22:06:08 -0700

    added tailwind, centered the map in flex box

--- src/MapView.js
+++ src/MapView.js
@@ -3,17 +3,22 @@ import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 const MapView = () => {
-    const position = [51.505, -0.09]; // Latitude and Longitude of London
+    const position = [40.00719953657654, -105.26308100217875]; // Latitude and Longitude of Engineering Center
 
     return (
-        <MapContainer center={position} zoom={13} style={{ height: '100vh', width: '100%' }}>
-            <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            />
-            {/* Points will be added here in the future */}
-        </MapContainer>
+        <div>
+            <div className="flex justify-center items-center w-screen h-screen">
+                <MapContainer center={position} zoom={13} style={{ height: '70vh', width: '70%' }}>
+                    <TileLayer
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    />
+                    {/* Points will be added here in the future */}
+                </MapContainer>
+            </div>
+        </div>
     );
 };
 
 export default MapView;
+
