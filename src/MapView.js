7320979783769a2e6259568e9dba244e823c3c5a
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2023-12-02 19:04:28 -0700

    playing around with the map to add points onMapClick event and move the Map to where the points are

--- src/MapView.js
+++ src/MapView.js
@@ -1,41 +1,51 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import React, { useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
 import { Icon } from 'leaflet';
 
 import 'leaflet/dist/leaflet.css';
 
+const customIcon = new Icon({
+    iconUrl: '/mapMarker.png', // Replace with the path to your image
+    iconSize: [25, 25], // Size of the icon
+});
 
+// Example data points
+const dataPoints = [
+    { id: 1, position: [40.007259752621174, -105.26350055410776], info: 'Engineering Center' },
+    { id: 2, position: [40.00389502140387, -105.26616659936727], info: 'Center For Community' },
+    // Add more points here
+];
 
 const MapView = () => {
-    const position = [40.00719953657654, -105.26308100217875]; // Latitude and Longitude of Engineering Center
+    const [mapCenter, setMapCenter] = useState([39.73887641288653, -104.98464946096463]);
+    const [position, setPosition] = useState([]);
 
-    const customIcon = new Icon({
-        iconUrl: '/mapMarker.png', // Replace with the path to your image
-        iconSize: [25, 25], // Size of the icon
-    });
+    function LocationMarker() {
+        const map = useMapEvents({
+            click() {
+                setPosition([...dataPoints]);
+                console.log(position);
+                setMapCenter([40.007259752621174, -105.26350055410776])
+                map.flyTo([40.007259752621174, -105.26350055410776], map.getZoom());
+            }
+        });
 
-    // Example data points
-    const dataPoints = [
-        { id: 1, position: [40.007259752621174, -105.26350055410776], info: 'Engineering Center' },
-        { id: 2, position: [40.00389502140387, -105.26616659936727], info: 'Center For Community' },
-        // Add more points here
-    ];
-
-
+        return position.length === 0 ? null : position.map((point) => (
+            <Marker key={point.id} position={point.position} icon={customIcon}>
+                <Popup>{point.info}</Popup>
+            </Marker>
+        ));
+    }
 
     return (
         <div>
             <div className="flex justify-center items-center w-screen h-screen">
-                <MapContainer center={position} zoom={13} style={{ height: '70vh', width: '70%' }}>
+                <MapContainer center={mapCenter} zoom={13} style={{ height: '70vh', width: '70%' }}>
                     <TileLayer
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     />
-                    {dataPoints.map(point => (
-                        <Marker key={point.id} position={point.position} icon={customIcon}>
-                            <Popup>{point.info}</Popup>
-                        </Marker>
-                    ))}
+                    <LocationMarker />
                 </MapContainer>
             </div>
         </div>
